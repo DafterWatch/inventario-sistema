9e3e5d1ea74b07463f214c8c60c5c9289cb2c0ca
DafterWatch <marcosingefa@gmail.com>
2023-03-19 19:53:46 -0400

    fix responsive navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { ProductsModule } from './products/products.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +10,6 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
     NavbarComponent,
     HomeComponent
   ],
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListComponent } from './list/list.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 
 @NgModule({
-  declarations: [ListComponent, ProductListComponent],
+  declarations: [ProductListComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule
