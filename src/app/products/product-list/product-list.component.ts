d379d68e80f4bfcefea5233857358b9fd8e95d89
DafterWatch <marcosingefa@gmail.com>
2023-03-19 22:50:20 -0400

    avance

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -8,14 +8,99 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
 
   products = [
-    { id: 1, name: 'Producto 1' },
-    { id: 2, name: 'Producto 2' },
-    { id: 3, name: 'Producto 3' }
+    {
+      id: 1,
+      name: 'Producto 1',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 1',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Producto 2',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
+    {
+      id: 3,
+      name: 'Producto 3',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
+    {
+      id: 4,
+      name: 'Producto 4',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
+    {
+      id: 5,
+      name: 'Producto 5',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
+    {
+      id: 6,
+      name: 'Producto 6',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
+    {
+      id: 7,
+      name: 'Producto 7',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
+    {
+      id: 8,
+      name: 'Producto 8',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
+    {
+      id: 9,
+      name: 'Producto 9',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
+    {
+      id: 10,
+      name: 'Producto 10',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
+    {
+      id: 11,
+      name: 'Producto 11',
+      imageUrl: 'https://via.placeholder.com/150',
+      description: 'Descripción del producto 2',
+      price: 200,
+    },
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  gridView: boolean = false;
+
+  setListView() {
+    this.gridView = false;
+  }
+
+
+  setGridView() {
+    this.gridView = true;
   }
 
 }
